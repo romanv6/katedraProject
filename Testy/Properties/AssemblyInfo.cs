bdae94680d001a31cdc475f3c81fea6f0c8c1cd3
wujekgrzyb/jacekd/folbo <szympans18@gmail.com>
2015-04-29 10:56:37 +0200

    Dodane projekt z testami

--- Testy/Properties/AssemblyInfo.cs
+++ Testy/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestyJednostkowe")]
+[assembly: AssemblyTitle("Testy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SGGW")]
-[assembly: AssemblyProduct("TestyJednostkowe")]
-[assembly: AssemblyCopyright("Copyright © SGGW 2015")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testy")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3bfbf1bd-fa11-47fd-86ea-5dad1b0c2b06")]
+[assembly: Guid("a376fc6e-4608-4ecd-a12e-df7655bbd3ae")]
 
 // Version information for an assembly consists of the following four values:
 //
