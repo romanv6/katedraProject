87c164adaba5a49b29f713e04d1207f5fb79f14c
samsungkotlin <c3755656@trbvm.com>
2015-05-16 16:07:19 +0200

    Zaktualizowana ListaPrzedmiotow
    Analogicznie do ListaProwadzacych

--- ZarzadzarnieKatedra/Lists/ListaPrzedmiotow.cs
+++ ZarzadzarnieKatedra/Lists/ListaPrzedmiotow.cs
@@ -1,39 +1,125 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class ListaPrzedmiotow {
-	/// <summary>
-	/// To jest lista objektów Przedmiot
-	/// </summary>
-	private List<Przedmiot> lista;
-	public List<Przedmiot> Lista {
-		get {
-			return lista;
-		}
-		set {
-			lista = value;
-		}
-	}
+namespace LPrzedmiotow
+{
+    class Przedmiot
+    {
 
-	/// <summary>
-	/// Konstruktor
-	/// </summary>
-	public ListaPrzedmiotow() {
-		throw new System.Exception("Not implemented");
-	}
-	/// <summary>
-	/// Funkcja wczytuje do listy przedmioty z pliku za pomoc¹ funkcji WczytajPrzedmioty() klasy statycznej WczytajPliki
-	/// </summary>
-	/// <param name="sciezka">Œcie¿ka do pliku z list¹ przedmiotów</param>
-	public List<Przedmiot> Wczytaj(string sciezka) {
-		throw new System.Exception("Not implemented");
-	}
-	/// <param name="sciezka">Œcie¿ka do pliku, w którym zapiszemy listê</param>
-	public void Zapisz(string sciezka) {
-		throw new System.Exception("Not implemented");
-	}
+    }
+    public class ListaPrzedmiotow : ICollection<Przedmiot>, IList<Przedmiot>
+    {
 
-	private Przedmiot attribute;
-	private Przedmiot Lista;
+        public List<Przedmiot> ListaPrzedmiotow;
 
+        public ListaPrzedmiotow()
+        {
+            ListaPrzedmiotow = new List<Przedmiot>();
+        }
+
+        public List<Przedmiot> SzukajProwadzacych(string tekst)
+        {
+            return ListaPrzedmiotow;
+        }
+
+        public List<Przedmiot> Wczytaj(string sciezka)
+        {
+            throw new System.Exception("Not implemented");
+        }
+
+        public void Zapisz(string sciezka)
+        {
+            throw new System.Exception("Not implemented");
+        }
+
+        public int Count //zwraca liczbê elementów lsty
+        {
+            get { return ListaPrzedmiotow.Count; }
+        }
+
+        public IEnumerator GetEnumerator() //do foreach'a
+        {
+            return ListaPrzedmiotow.GetEnumerator(); //
+        }
+
+        public int IndexOf(Przedmiot item) //przesy³a przedmiot i zwraca jego index
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (item == ListaPrzedmiotow[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, Przedmiot item)
+        {
+            ListaPrzedmiotow.Insert(index, item);
+        }
+
+        public void RemoveAt(int index) //do usuwania elementu z przes³anego indeksu
+        {
+            ListaPrzedmiotow.RemoveAt(index);
+        }
+
+        public Przedmiot this[int index]
+        {
+            get
+            {
+                return ListaPrzedmiotow[index];
+            }
+            set
+            {
+                ListaPrzedmiotow[index] = value;
+            }
+        }
+
+        public void Add(Przedmiot item)
+        {
+            ListaPrzedmiotow.Add(item);
+        }
+
+        public void Clear()
+        {
+            ListaPrzedmiotow.Clear();
+        }
+
+        public bool Contains(Przedmiot item)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (item == ListaPrzedmiotow[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void CopyTo(Przedmiot[] array, int arrayIndex) //kopiuje do okreœlonego indeksu, obecnie nie potrzebujemy
+        {
+            ListaPrzedmiotow.CopyTo(array, arrayIndex);
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(Przedmiot item) // usuwa obiekty prowadz¹cy
+        {
+            return ListaPrzedmiotow.Remove(item);
+        }
+
+        IEnumerator<Przedmiot> IEnumerable<Przedmiot>.GetEnumerator()
+        {
+            return ListaPrzedmiotow.GetEnumerator();
+        }
+    }
 }
